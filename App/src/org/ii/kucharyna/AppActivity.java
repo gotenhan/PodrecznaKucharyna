bff533d9cf4d1590eaf15ceba3ba7239a1b858df
Adrian Dyliński <gotenhan@gmail.com>
2012-05-13 16:01:19 +0200

    Create skeleton for application

--- App/src/org/ii/kucharyna/AppActivity.java
+++ App/src/org/ii/kucharyna/AppActivity.java
@@ -1,13 +1,47 @@
 package org.ii.kucharyna;
 
+import java.util.ArrayList;
+
+import org.ii.kucharyna.presenter.MainPresenter;
+import org.ii.kucharyna.presenter.views.IRecipeView;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class AppActivity extends Activity {
+public class AppActivity extends Activity implements IRecipeView{
     /** Called when the activity is first created. */
+	private MainPresenter presenter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        this.presenter = new MainPresenter();
     }
+
+	public void setRecipeName(String recipeName) {
+		final String _recipeName = recipeName;
+		this.runOnUiThread(new Runnable(){
+			public void run(){
+				TextView recipeNameTextView = (TextView)findViewById(R.id.recipe_name);
+				recipeNameTextView.setText(_recipeName);
+			}
+		});
+	}
+
+	public void setRecipeDescription(String description) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setRecipeInstructions(String instructions) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setRecipeTags(ArrayList<String> tags) {
+		// TODO Auto-generated method stub
+		
+	}
+    
 }
---
+++ persistance/src/main/java/org/ii/kucharyna/persistance/models/RecipeModel.java
@@ -0,0 +1,52 @@
+package org.ii.kucharyna.persistance.models;
+
+import java.util.ArrayList;
+
+import org.bson.types.ObjectId;
+
+public class RecipeModel {
+	
+	private ObjectId _id = new ObjectId();
+	private String description = null;
+	private String name = null;
+	private String instructions = null;
+	private ArrayList<String> tags = new ArrayList<String>();
+	//private IngredientModel[] ingredients;
+	
+	public ObjectId get_id() {
+		return _id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public String getInstructions() {
+		return instructions;
+	}
+	public String getName() {
+		return name;
+	}
+	public ArrayList<String> getTags() {
+		return tags;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public void setInstructions(String instructions) {
+		this.instructions = instructions;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setTags(ArrayList<String> tags) {
+		this.tags = tags;
+	}
+	
+	public void addTag(String tag){
+		if(tag != null)
+			tags.add(tag);
+	}
+	public void removeTag(String tag){
+		if(tag != null)
+			tags.remove(tag);
+	}
+}
