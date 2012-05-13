f0ba8a076744021568a43238c505e2c3fe928c54
Adrian Dyliński <gotenhan@gmail.com>
2012-05-13 16:08:22 +0200

    Move presenter to PodrecznaKucharyna's directory

---
+++ presenter/src/org/ii/kucharyna/presenter/views/IRecipeView.java
@@ -0,0 +1,10 @@
+package org.ii.kucharyna.presenter.views;
+
+import java.util.ArrayList;
+
+public interface IRecipeView {
+	public void setRecipeName(String name);
+	public void setRecipeDescription(String description);
+	public void setRecipeInstructions(String instructions);
+	public void setRecipeTags(ArrayList<String> tags);
+}
