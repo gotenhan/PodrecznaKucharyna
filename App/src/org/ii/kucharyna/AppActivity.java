8790bfdcd9c15f15137d4f01c8c586db1dc4b12a
Adrian Dyliński <gotenhan@gmail.com>
2012-05-16 17:14:14 +0200

    Configure project to compile and run on emulator

--- App/src/org/ii/kucharyna/AppActivity.java
+++ App/src/org/ii/kucharyna/AppActivity.java
@@ -9,21 +9,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class AppActivity extends Activity implements IRecipeView{
-    /** Called when the activity is first created. */
+public class AppActivity extends Activity implements IRecipeView {
+	/** Called when the activity is first created. */
 	private MainPresenter presenter;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        this.presenter = new MainPresenter();
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		this.presenter = new MainPresenter();
+		presenter.refreshRecipe(this, 1);
+	}
 
 	public void setRecipeName(String recipeName) {
 		final String _recipeName = recipeName;
-		this.runOnUiThread(new Runnable(){
-			public void run(){
-				TextView recipeNameTextView = (TextView)findViewById(R.id.recipe_name);
+		this.runOnUiThread(new Runnable() {
+			public void run() {
+				TextView recipeNameTextView = (TextView) findViewById(R.id.recipe_name);
 				recipeNameTextView.setText(_recipeName);
 			}
 		});
@@ -31,17 +32,17 @@ public class AppActivity extends Activity implements IRecipeView{
 
 	public void setRecipeDescription(String description) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void setRecipeInstructions(String instructions) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void setRecipeTags(ArrayList<String> tags) {
 		// TODO Auto-generated method stub
-		
+
 	}
-    
+
 }
--- persistance/src/main/java/org/ii/kucharyna/persistance/models/RecipeModel.java
+++ persistance/src/main/java/org/ii/kucharyna/persistance/models/RecipeModel.java
@@ -4,49 +4,66 @@ import java.util.ArrayList;
 
 import org.bson.types.ObjectId;
 
-public class RecipeModel {
-	
+public class RecipeModel { 
+
 	private ObjectId _id = new ObjectId();
 	private String description = null;
 	private String name = null;
 	private String instructions = null;
 	private ArrayList<String> tags = new ArrayList<String>();
-	//private IngredientModel[] ingredients;
-	
+
+	// private IngredientModel[] ingredients;
+
+	public RecipeModel() {
+	}
+
+	public void addTag(String tag) {
+		if (tag != null)
+			tags.add(tag);
+	}
+
 	public ObjectId get_id() {
 		return _id;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public String getInstructions() {
 		return instructions;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public ArrayList<String> getTags() {
 		return tags;
 	}
+
+	public void removeTag(String tag) {
+		if (tag != null)
+			tags.remove(tag);
+	}
+
+	public void set_id(ObjectId _id) {
+		this._id = _id;
+	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public void setInstructions(String instructions) {
 		this.instructions = instructions;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public void setTags(ArrayList<String> tags) {
 		this.tags = tags;
-	}
-	
-	public void addTag(String tag){
-		if(tag != null)
-			tags.add(tag);
-	}
-	public void removeTag(String tag){
-		if(tag != null)
-			tags.remove(tag);
 	}
 }
